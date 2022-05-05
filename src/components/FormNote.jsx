338032bdf24d63d48b8a83af0de8ed27274b7f28
Manuel Pineda <pineda.manuel@gmail.com>
2022-05-05 17:52:56 -0500

    FormNote ----- Note form implementation. Tested

---
+++ src/components/FormNote.jsx
@@ -0,0 +1,37 @@
+import { useContext, useState } from "react"
+import { Store } from "../state/StoreProvider";
+
+const FormNote = () => {
+
+  
+
+  const [note, setNote] = useState('');
+
+  const onAdd = (event)=>{
+    event.preventDefault()
+    if(note){
+      dispatch({
+        type: 'add-note',
+        payload: {
+          note
+        }
+      })
+    }
+  }
+
+  const {state, dispatch} = useContext(Store)
+
+  const addNote = (event) =>{
+    const entryNote = event.target.value
+    setNote(entryNote)
+  }
+
+  return (
+    <form>
+     <input onChange={addNote} type="text" name="note" placeholder="Note"/>
+     <button onClick={onAdd}>Add a new note!</button>
+   </form>
+  )
+}
+
+export default FormNote
