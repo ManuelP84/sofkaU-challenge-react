a6fed11ce7afcc6b6a887fb31bb8102510110ddb
Manuel Pineda <pineda.manuel@gmail.com>
2022-05-05 17:45:29 -0500

    FormCategory ----- Category form implementation. Tested

---
+++ src/components/FormCategory.jsx
@@ -0,0 +1,35 @@
+import { useContext, useState } from "react"
+import { Store } from "../state/StoreProvider";
+
+const FormCategory = () => {
+
+  const [category, setCategory] = useState('')
+
+  const {state, dispatch} = useContext(Store)
+
+  const onAdd = (event)=>{
+    event.preventDefault()
+    if(category){
+      dispatch({
+        type: 'add-category',
+        payload: {
+          category
+        }
+      })
+    }
+  }
+
+  const addCategory = (event) =>{
+    const entryCategory = event.target.value
+    setCategory(entryCategory)
+  }
+
+  return (
+   <form>
+     <input onChange={addCategory} type="text" name="category" placeholder="Category"/>
+     <button onClick={onAdd}>New Category</button>
+   </form>
+  )
+}
+
+export default FormCategory
