bb3c5633b11ff51764e9a77d843ad005fb83a64d
Manuel Pineda <pineda.manuel@gmail.com>
2022-05-06 02:05:27 -0500

    Reducer   ---- Delete category function working

--- src/state/Reducer.jsx
+++ src/state/Reducer.jsx
@@ -1,14 +1,23 @@
-function reducer(state, action){
+
+function reducer(state, action){    
     switch(action.type){
         case 'add-note':
+            //Logica
+            console.log("Adding a note!!!")
             return state
-        case 'remove-note':
-            return state
-        case 'update-note':
+        case 'delete-note':
+            console.log("Deleting note!!!")
+        case 'update-note':            
             return state
         case 'add-category':
+            console.log("Adding a category!!!")
             return state
-        case 'delete-category':
-            return state
+        case 'delete-category':            
+            const newListOfCategories = state.listOfCategories.filter(Category => Category.id !== action.payload.id)
+            console.log(newListOfCategories)
+            const newStateCategoryDeleted = {...state, listOfCategories: newListOfCategories}
+            return newStateCategoryDeleted
     }
 }
+
+export default reducer
