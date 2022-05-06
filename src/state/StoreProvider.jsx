0902e1b3d7328c08ff341766e7a5986035db54df
Manuel Pineda <pineda.manuel@gmail.com>
2022-05-06 10:51:51 -0500

    StoreProvider ---- Change the initial state structure

--- src/state/StoreProvider.jsx
+++ src/state/StoreProvider.jsx
@@ -1,61 +1,49 @@
 import React, { createContext, useReducer } from "react";
-import reducer from "./Reducer"
+import reducer from "./Reducer";
 
-const initialState = {
-  category: {
-    id: "",
-    name: "",
+const initialState = [
+  {
+    id: "0",
+    name: "Category by default 0",
     notes: [
       {
-        id: "",
-        title: "",
+        id: "0",
+        title: "Title by default 0",
         done: false,
-        fkCategoryId: "",
+        fkCategoryId: "0",
       },
     ],
   },
-  listOfCategories: [
-    {
-      id: "0",
-      name: "Category by default 0",
-      notes: [
-        {
-          id: "0",
-          title: "Title by default 0",
-          done: false,
-          fkCategoryId: "0",
-        },
-      ],
-    },
-    {
-      id: "1",
-      name: "Category by default 1",
-      notes: [
-        {
-          id: "1",
-          title: "Title by default 1",
-          done: false,
-          fkCategoryId: "1",
-        },
-        {
-          id: "2",
-          title: "Title by default 1",
-          done: false,
-          fkCategoryId: "1",
-        },
-      ],
-    },
-  ],
-};
+  {
+    id: "1",
+    name: "Category by default 1",
+    notes: [
+      {
+        id: "1",
+        title: "Title by default 1",
+        done: false,
+        fkCategoryId: "1",
+      },
+      {
+        id: "2",
+        title: "Title by default 1",
+        done: false,
+        fkCategoryId: "1",
+      },
+    ],
+  },
+];
 
 const Store = createContext(initialState);
 
 const StoreProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  return <Store.Provider value={{state, dispatch}}>{children}</Store.Provider>;
+  return (
+    <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>
+  );
 };
 
 export default StoreProvider;
 
-export {Store, initialState}
+export { Store, initialState };
