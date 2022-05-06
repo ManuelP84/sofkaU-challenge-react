5cc96668846d8cf89e33f8837e21f68fc1ac0f2b
Manuel Pineda <pineda.manuel@gmail.com>
2022-05-06 02:16:47 -0500

    Initial state modification ---- More default categories included

--- src/state/StoreProvider.jsx
+++ src/state/StoreProvider.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useReducer } from "react";
+import reducer from "./Reducer"
 
 const initialState = {
   category: {
@@ -16,13 +17,31 @@ const initialState = {
   listOfCategories: [
     {
       id: "0",
-      name: "Category by default",
+      name: "Category by default 0",
       notes: [
         {
           id: "0",
-          title: "Title by default",
+          title: "Title by default 0",
           done: false,
           fkCategoryId: "0",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "Category by default 1",
+      notes: [
+        {
+          id: "1",
+          title: "Title by default 1",
+          done: false,
+          fkCategoryId: "1",
+        },
+        {
+          id: "2",
+          title: "Title by default 1",
+          done: false,
+          fkCategoryId: "1",
         },
       ],
     },
@@ -34,7 +53,9 @@ const Store = createContext(initialState);
 const StoreProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  return <Store.Provider>{children}</Store.Provider>;
+  return <Store.Provider value={{state, dispatch}}>{children}</Store.Provider>;
 };
 
 export default StoreProvider;
+
+export {Store, initialState}
